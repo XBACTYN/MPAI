79f66c0871a931f641c5fdcf27f778e7bd8a9837
XBACTYN <dreamquarz@gmail.com>
2022-02-15 12:34:05 +0400

    Проверка загрузки изображения. Вывод параметров изображения.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import json
+from skimage.io import imread, imsave
 
-def print_hi(name):
-    print(f'Hi, {name}')
+settings = {
+    'source_path': 'C:/Users/user/Desktop/rainbow.jpg',
+    'result_path': '',
+    'positionX': 30,
+    'positionY': 30,
+    'color': ''
+}
 
+with open ('settings.json', 'w') as fp:
+    json.dump(settings, fp)
 
+with open ('settings.json') as json_file:
+    json_data = json.load(json_file)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for entry in json_data.keys():
+    print(f'Название параметра: {entry},тип параметра: {type(json_data[entry])}, значение параметра: {json_data[entry]}')
 
+path = json_data['source_path']
+img = imread(path)
+print('Image shape: ', img.shape)
 
